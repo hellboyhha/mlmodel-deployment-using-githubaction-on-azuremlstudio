71780748e2fab65a28d4a77044e07b309521aa2f
Hein Htet Aung <57698984+hellboyhha@users.noreply.github.com>
2023-09-05 20:36:21 +0630

    Update model-deployment-azure.py

--- model-deployment-azure.py
+++ model-deployment-azure.py
@@ -1,18 +1,3 @@
-import os
-
-# details of Azure Machine Learning workspace
-subscription_id = os.environ['AZ_SUBSCRIPTION_ID']
-resource_group = os.environ['AZ_RESOURCE_GP_NAME']
-workspace_name = os.environ['AZ_ML_WORKSPACE_NAME']
-
-print(subscription_id)
-print(resource_group)
-print(workspace_name)
-[hellboyhha@EMPT1NES2:~/ml-demo]$ ls
-model-deployment-azure.py  print_env.py  wine.zip
-[hellboyhha@EMPT1NES2:~/ml-demo]$ ls
-model-deployment-azure.py  print_env.py  wine.zip
-[hellboyhha@EMPT1NES2:~/ml-demo]$ cat model-deployment-azure.py
 # import required libraries
 import os
 from azure.ai.ml import MLClient
